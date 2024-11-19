0a8c2062e7ce60204aadeaadcc90eee850774f2a
ismaelvr1999 <ismael.villalpando@alumnos.udg.mx>
2024-11-19 17:48:55 -0600

    task 10 to 13 done

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,7 +6,7 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+
   const result = users.find(user => user.username === username);
   if (typeof result === "undefined"){
     return false
@@ -15,7 +15,7 @@ const isValid = (username)=>{ //returns boolean
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+
   const result = users.find(user => user.username === username && user.password === password)
   if(typeof result === "undefined"){
     return false;
@@ -25,7 +25,7 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
+ 
   const {username, password} = req.body;
   if(!authenticatedUser(username,password)){
     
@@ -38,7 +38,7 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
+  
   const {review} = req.body;
   const {isbn} = req.params;
   const {username} = req.session.authorization
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,42 +22,72 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  const bookList = JSON.stringify(books,null,1);
-  return res.status(200).json(bookList);
+public_users.get("/", function (req, res) {
+  const getBookList = new Promise((resolve, reject) => {
+    const bookList = JSON.stringify(books, null, 1);
+    resolve(bookList);
+  });
+
+  getBookList
+    .then((bookList) => res.status(200).json(bookList))
+    .catch((error) =>
+      res.status(500).json({ message: "Internal Server Error" })
+    );
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  const {isbn} = req.params;
-  return res.status(200).json(books[isbn]);
+  const getBookDetails = new Promise((resolve,reject)=>{
+    const {isbn} = req.params;
+    const bookDetails = books[isbn];
+    resolve(bookDetails);
+  });
+
+  getBookDetails.then(bookDetails => res.status(200).json(bookDetails))
+    .catch(error => res.status(500).json({message: "Internal Server Error" }));
+  //const {isbn} = req.params;
+  //return res.status(200).json(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  const {author} = req.params;
-  let booksArray = Object.values(books);
-  let authorBooks = booksArray.filter(element => element.author === author)
 
-  return res.status(200).json(authorBooks);
+  const {author} = req.params;
+  const getBookDetails = new Promise((resolve,reject)=>{
+    let booksArray = Object.values(books);
+    let authorBooks = booksArray.filter(element => element.author === author);
+
+    resolve(authorBooks);
+  })
+  getBookDetails.then(authorBooks => res.status(200).json(authorBooks))
+    .catch(error => res.status(500).json({message: "Internal Server Error" }));
+  //let booksArray = Object.values(books);
+  //let authorBooks = booksArray.filter(element => element.author === author)
+
+  //return res.status(200).json(authorBooks);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
+  
   const {title} = req.params;
-  let booksArray = Object.values(books);
-  let titleBook = booksArray.find(element => element.title === title )
+  const getTitleBook = new Promise((resolve,reject)=>{
+    let booksArray = Object.values(books);
+    let titleBook = booksArray.find(element => element.title === title );
+    resolve(titleBook);
+  });
 
-  return res.status(200).json(titleBook);
+  getTitleBook.then(books => res.status(200).json(books))
+    .catch(error => res.status(500).json({message: "Internal Server Error" }));
+  //let booksArray = Object.values(books);
+  //let titleBook = booksArray.find(element => element.title === title )
+
+  //return res.status(200).json(titleBook);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
+
   const {isbn} = req.params;
   const bookReview = books[isbn].reviews
   return res.status(200).json({reviews: bookReview});
